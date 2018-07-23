c0794b2730a5b7d9dd410b36b804f47d8becf819
netflair <dev@netflair.at>
2018-07-23 23:01:35 +0200

    added profile page

---
+++ dashboard/profile.php
@@ -0,0 +1,13 @@
+<?php include('inc/head.php'); ?>
+
+<div class="admin-content">
+    <!-- Content User-->
+    Display all users in Table, for edit, ban, or remove. 
+</div>
+
+<div class="user-content">
+    <!-- Content User-->
+    Display user profile for edit. Or remove account. 
+</div>
+
+<?php include('inc/footer.php'); ?>
