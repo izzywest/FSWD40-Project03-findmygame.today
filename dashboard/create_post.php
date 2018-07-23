afcdd9bc627f080bd7868da9176275fdf88af96a
netflair <dev@netflair.at>
2018-07-23 23:28:39 +0200

    added pages

---
+++ dashboard/create_post.php
@@ -0,0 +1,9 @@
+<?php $pageTitle = 'Create Post' ?>
+<?php include('inc/head.php'); ?>
+
+<div class="user-content">
+    <!-- Content User-->
+    Form to create new Post
+</div>
+
+<?php include('inc/footer.php'); ?>
---
+++ dashboard/edit_post.php
@@ -0,0 +1,9 @@
+<?php $pageTitle = 'Edit Post' ?>
+<?php include('inc/head.php'); ?>
+
+<div class="user-content">
+    <!-- Content User and Admin-->
+    Form to edit Post by current Post ID
+</div>
+
+<?php include('inc/footer.php'); ?>
