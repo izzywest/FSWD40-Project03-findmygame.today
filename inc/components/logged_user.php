1b97756c1bfbbdd169456334e12ab8bc05497e2c
netflair <dev@netflair.at>
2018-07-23 22:24:10 +0200

    removed, not needed

--- inc/components/logged_user.php
+++
@@ -1,3 +0,0 @@
-<div class="user-logged">
-            <img class="avatar-small" src="../img/avatars/default_avatar.jpg" alt="avatar"> <span class="username">$Username</span> <a href="#logout">Logout</a>
-</div>
