48d36e770e8ac2398a0a7f7c593dece8758ec891
netflair <dev@netflair.at>
2018-07-23 22:59:58 +0200

    added post page

---
+++ dashboard/posts.php
@@ -0,0 +1,13 @@
+<?php include('inc/head.php'); ?>
+
+<div class="admin-content">
+    <!-- Content Admin -->
+    Posts from all users to manage (edit, delete)
+</div>
+
+<div class="user-content">
+    <!-- Content User-->
+    User own posts to edit, delete
+</div>
+
+<?php include('inc/footer.php'); ?>
