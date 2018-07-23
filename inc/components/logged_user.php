ef629a3f4f9f80358bd6458ca1e66e6e3b4f7103
netflair <dev@netflair.at>
2018-07-23 13:45:43 +0200

    added component

---
+++ inc/components/logged_user.php
@@ -0,0 +1,3 @@
+<div class="user-logged">
+            <img class="avatar-small" src="../img/avatars/default_avatar.jpg" alt="avatar"> <span class="username">$Username</span> <a href="#logout">Logout</a>
+</div>
