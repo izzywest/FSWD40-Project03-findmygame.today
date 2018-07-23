efa9ae60616184bdbae6ae76efbb30e5524cb98b
netflair <dev@netflair.at>
2018-07-23 23:18:04 +0200

    added status

--- dashboard/inc/footer.php
+++ dashboard/inc/footer.php
@@ -1,10 +1,10 @@
     </main>
     <footer>
         <ul class="status-bar">
+            <li>Logged in as: $role</li>
             <li>Rank: $rank</li>
             <li>Posts: <a href="posts.php">$int</a></li>
             <li>Favorite Games: $int</li>
-            <li></li>
         </ul>
         <div class="logout-container">
             <span class="icon-small"><?php echo file_get_contents("../img/icons/exit-door.svg"); ?></span>
