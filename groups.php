9cc1d358fe2eda31133cbd1dd91054ff1afb0b45
netflair <dev@netflair.at>
2018-07-24 22:32:34 +0200

    create groups page

---
+++ groups.php
@@ -0,0 +1,13 @@
+<?php include('inc/head.php'); ?>
+   <div id="about" class="background-image"></div> 
+    <!--Content-->
+    <main class="top-margin">
+        <div class="content">
+            <h1>Groups</h1>
+            here goes the contetn
+        </div>
+        <aside>
+            Display w/e data here
+        </aside> 
+    </main>
+<?php include('inc/footer.php'); ?>
