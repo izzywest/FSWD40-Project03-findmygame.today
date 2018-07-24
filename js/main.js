dc7f7d07e457167552de45d536ed99dcfb4f49f9
netflair <dev@netflair.at>
2018-07-24 18:48:51 +0200

    added margin for slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 jQuery(document).ready(function($) {
 
+    //Margin because of fixed Header
+    var headerHeight = $('header').height();
+    $('#slider').css('margin-top', 'calc(50px + ' + headerHeight + 'px)');
+
     //Slider
     $('#slider').basicSlider();
 
