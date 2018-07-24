1038216aa12c86a6f57fb46b83516524e79cd947
netflair <dev@netflair.at>
2018-07-24 20:26:23 +0200

    added margin for pages without slider

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function($) {
     //Margin because of fixed Header
     var headerHeight = $('header').height();
     $('#slider').css('margin-top', 'calc(50px + ' + headerHeight + 'px)');
+    $('.top-margin').css('margin-top', 'calc(50px + ' + headerHeight + 'px)');
 
     //Slider
     $('#slider').basicSlider();
