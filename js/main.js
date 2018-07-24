05daa545a7de6c31b3b224e98988ed6f8f7907b9
netflair <dev@netflair.at>
2018-07-24 22:35:31 +0200

    added function to mark current menu item as active based on URL

--- js/main.js
+++ js/main.js
@@ -8,4 +8,17 @@ jQuery(document).ready(function($) {
     //Slider
     $('#slider').basicSlider();
 
+    //Active Menu
+    // Get current path and find target link
+    var path = window.location.pathname.split("/").pop();
+    
+    // Account for home page with empty path
+    if ( path == '' ) {
+        path = 'index.php';
+    }
+        
+    var target = $('nav a[href="'+path+'"]');
+    // Add active class to target link
+    target.addClass('active');
+
 });
