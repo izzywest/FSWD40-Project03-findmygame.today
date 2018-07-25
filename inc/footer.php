cee6bb5180ef3a2fa61af9f2b428ab180da1ca26
schahnaz <schahnaz@mail.de>
2018-07-25 16:25:53 +0200

    add thisshit

--- inc/footer.php
+++ inc/footer.php
@@ -1,18 +1,18 @@
 <style>
 
 footer {
-    /* padding: 0 !important; */
 }
 
 .footer-img {
     width: 3.5rem;
 }
+
 .socialbar {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 2fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr;
   align-items: center;
-  /* justify-items: center;  */
+  margin: 1em;
 }
 
 .social-item {
@@ -22,22 +22,43 @@ justify-self: center;
 .info-container {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  /* grid-template-rows: 1fr 1fr 1fr; */
+  margin: 1em;
   align-items: center;
 }
 
 .info-item {
 justify-self: center;
+text-align: center;
 }
 
+.contact-img {
+    width: 1.5em;   
+}
+
+@media only screen and (max-width: 600px) {
+    .socialbar {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-template-rows: 1fr;
+        align-items: center;
+        margin: 1em
+}
+    .info-container {
+        display: grid;
+        grid-template-columns: 1fr;
+        margin: 1em;
+        align-items: center;
+    }
+    .info-item {
+        margin-bottom: 1.5em;
+    }
+}
 
 </style>    
 
     <!--Footer-->
     <footer>
         <div class="socialbar">
-                <div class="social-item footer-img">
-                </div>
                 <div class="social-item footer-img">
                 <?php echo file_get_contents("img/icons/social/discord.svg"); ?>
                 </div>
@@ -58,19 +79,21 @@ justify-self: center;
                 </div>
                 <div class="social-item footer-img">
                 <?php echo file_get_contents("img/icons/social/deviantart.svg"); ?>
-                </div>       
+                </div>
                 <div class="social-item footer-img">
-                </div>     
+                <?php echo file_get_contents("img/icons/social/facebook.svg"); ?>
+                </div>
         </div>
+        <hr>
         <div class="info-container">
-            <div class="info-item">
-                <p><b>Address:</b><br> Gamerstreet 20,<br> 23932 San Andreas</p>
+            <div class="contact-img info-item">
+                <span><?php echo file_get_contents("img/icons/social/envelope.svg"); ?><b>info@findmygame.com</b></span>
             </div>
-            <div class="info-item">
-                <p><b>Address:</b><br> Gamerstreet 20,<br> 23932 San Andreas</p>
+            <div class="contact-img info-item">
+                <p><?php echo file_get_contents("img/icons/social/phone.svg"); ?><b>(369) 456-4866</b></p>
             </div>
-            <div class="info-item">
-                <p><b>Address:</b><br> Gamerstreet 20,<br> 23932 San Andreas</p>
+            <div class="contact-img info-item">
+                <p><?php echo file_get_contents("img/icons/social/home.svg"); ?><b></b>Gamerstreet 42, 06232 San Andreas</p>
             </div>
         </div>
 </div>
