dd31e4472b9a98c2035fcbe76d0c8d54ef5e2008
schahnaz <schahnaz@mail.de>
2018-07-25 09:15:44 +0200

    add footer, add icons

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,58 @@
+<style>
+
+footer {
+    /* padding: 0 !important; */
+}
+
+.footer-img {
+    width: 3.5rem;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  align-items: center;
+  justify-items: center; 
+}
+
+.grid-item {
+justify-self: center;
+}
+</style>    
+
     <!--Footer-->
     <footer>
-        Footer
+        <div class="grid-container">
+            <div class="grid-item footer-img">
+            </div>
+            <div class="grid-item footer-img">
+            <?php echo file_get_contents("img/icons/social/discord.svg"); ?>
+            </div>
+            <div class="grid-item footer-img">
+            <?php echo file_get_contents("img/icons/social/reddit.svg"); ?>
+            </div>
+            <div class="grid-item footer-img">
+            <?php echo file_get_contents("img/icons/social/steam.svg"); ?>
+            </div>
+            <div class="grid-item footer-img">
+            <?php echo file_get_contents("img/icons/social/telegram.svg"); ?>
+            </div>
+            <div class="grid-item footer-img">
+            <?php echo file_get_contents("img/icons/social/twitch.svg"); ?>
+            </div>
+            <div class="grid-item footer-img">
+            <?php echo file_get_contents("img/icons/social/youtube.svg"); ?>
+            </div>
+            <div class="grid-item footer-img">
+            <?php echo file_get_contents("img/icons/social/deviantart.svg"); ?>
+            </div>       
+            <div class="grid-item footer-img">
+            </div>     
+        </div>
+
+        <!-- <h1>Test</h1> -->
     </footer>
 
-    <!--Scritps-->
+    <!--Scripts-->
     <script src="js/sliders.js"></script>
     <script src="js/main.js"></script>
 </body>
