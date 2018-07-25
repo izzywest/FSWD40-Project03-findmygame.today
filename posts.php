6e8efff2908b786458f3d114e87e68ed679252c9
Patrick Klostermann <dev@netflair.at>
2018-07-25 21:32:16 +0200

    added posts to page

--- posts.php
+++ posts.php
@@ -1,11 +1,44 @@
 <?php $pageTitle='Posts'; ?>
 <?php include('inc/head.php'); ?>
+<?php include('config/Database.php'); ?>
+<?php
+//Show latest posts
+        $db = new Database;
+
+		$db->query('SELECT  `posts`.`PO_Headline`, 
+                            `posts`.`FK_U_ID`,
+                            `posts`.`PO_Description`,
+                            `posts`.`FK_GA_ID`,
+                            `posts`.`PO_DateTime`,
+                            `user`.`U_ID`,
+                            `user`.`U_Username`, 
+                            `game`.`GA_ID`,
+                            `game`.`GA_Name`
+                    FROM `posts`
+                    JOIN `user`
+                    ON   `posts`.`FK_U_ID` = `user`.`U_ID`
+                    JOIN `game`
+                    ON   `posts`.`FK_GA_ID` = `game`.`GA_ID`
+                    ORDER BY `posts`.`PO_DateTime` DESC
+                    LIMIT 10');
+		$showPosts = $db->getResultSet();
+?>
+
+
+
    <div id="about" class="background-image"></div> 
     <!--Content-->
     <main class="top-margin">
-        <div class="content">
-            <h1>Posts</h1>
-            here goes the contetn
+        <div id="posts" class="content">
+            <?php foreach($showPosts as $post) : ?>
+                    <div class="latest-posts post-boxes">
+                        <h3><?php echo $post['PO_Headline'] ?><button>contact</button></h3>
+                        <small><?php echo $post['PO_Description']; ?></small>
+                        <div class="user">
+                            <span><strong>By:</strong> <?php echo $post['U_Username']; ?> <strong>@</strong><?php echo $post['PO_DateTime']; ?></span> <strong class="small-game"><?php echo $post['GA_Name']; ?></strong>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
         </div>
         <aside>
             Display w/e data here
