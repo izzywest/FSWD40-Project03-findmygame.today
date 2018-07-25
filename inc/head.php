2e878cc7709d09aa9236833b9ca2a36bb8c1a3f9
Patrick Klostermann <dev@netflair.at>
2018-07-25 11:06:39 +0200

    added php title; added new font; placeholder login;

--- inc/head.php
+++ inc/head.php
@@ -9,10 +9,11 @@
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
         crossorigin="anonymous"></script>
     <!--Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css?family=Aldrich|Hammersmith+One|News+Cycle:400,700|Pragati+Narrow:400,700" rel="stylesheet">
     <!--Style-->
     <link rel="stylesheet" href="style/css/main.css">
-    <title>G4M3R5</title>
+    <title>G4M3R5 | <?php echo $pageTitle; ?></title>
 </head>
 
 <body>
@@ -21,9 +22,13 @@
         <div id="logo">
             <img src="img/logo.png" alt="logo">
         </div>
-
+        <h1 class="pageTitle"><?php echo $pageTitle; ?></h1>
         <nav>
             <ul>
+                <li><input type="text" class="css-input" placeholder="Username"></li>
+                <li><input type="password" class="css-input" placeholder="Password"></li>
+                <li><input class="btn" type="submit" value="Login"></li>
+                <li><input class="btn" type="submit" value="Logout"></li>
                 <li> <a href="games.php"> <span class="nav-img"> <?php echo file_get_contents("img/icons/game.svg"); ?> </span>Games </a></li>
                 <li> <a href="posts.php"> <span class="nav-img"> <?php echo file_get_contents("img/icons/posts.svg"); ?> </span>Posts </a></li>
                 <li> <a href="groups.php"> <span class="nav-img"> <?php echo file_get_contents("img/icons/group.svg"); ?> </span>Groups </a></li>
