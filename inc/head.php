2f86cc758979045f265eb753fa7ef4bd523f44ce
Patrick Klostermann <dev@netflair.at>
2018-07-26 08:09:15 +0200

    renamed groups to events

--- inc/head.php
+++ inc/head.php
@@ -33,7 +33,7 @@
             <ul>
                 <li> <a href="games.php"> <span class="nav-img"> <?php echo file_get_contents("img/icons/game.svg"); ?> </span>Games </a></li>
                 <li> <a href="posts.php"> <span class="nav-img"> <?php echo file_get_contents("img/icons/posts.svg"); ?> </span>Posts </a></li>
-                <li> <a href="groups.php"> <span class="nav-img"> <?php echo file_get_contents("img/icons/group.svg"); ?> </span>Groups </a></li>
+                <li> <a href="events.php"> <span class="nav-img"> <?php echo file_get_contents("img/icons/group.svg"); ?> </span>Events </a></li>
                 <li> <a href="about.php"> <span class="nav-img"> <?php echo file_get_contents("img/icons/user.svg"); ?> </span>About </a></li>
             </ul>
         </nav>
