e2dda938b26c6e4ee474aa492f2f77617c6c29fd
Patrick Klostermann <dev@netflair.at>
2018-07-26 08:06:12 +0200

    added events and images fro sidebar

--- events.php
+++ events.php
@@ -6,27 +6,45 @@
         $db = new Database;
 
 		$db->query('SELECT  *
-                    FROM `groups`
+                    FROM `event`
+                    JOIN `user`
+                    ON   `event`.`FK_U_ID` = `user`.`U_ID`
                    ');
-		$showPosts = $db->getResultSet();
+		$showEvents = $db->getResultSet();
 ?>
 
    <div id="about" class="background-image"></div> 
     <!--Content-->
     <main class="top-margin">
         <div id="posts" class="content">
-            <?php foreach($showPosts as $post) : ?>
+            <?php foreach($showEvents as $event) : ?>
                     <div class="latest-posts post-boxes">
-                        <h3><?php echo $post['GR_Name'] ?><button>contact</button></h3>
-                        <small><?php echo $post['GR_Capacity']; ?></small>
+                        <h3><?php echo $event['EV_Name'] ?><a href="https://www.google.com/maps/place/<?php echo $event['EV_Location']; ?>" target="_blank"><?php echo $event['EV_Location']; ?></a></h3>
+                        <small><?php echo $event['EV_Description']; ?></small>
+                        <div class="timeframe">
+                            <span><strong>Start: </strong><?php echo $event['EV_ST_DateTime']; ?></span><span><strong>End: </strong><?php echo $event['EV_END_DateTime']; ?></span>
+                        </div>
                         <div class="user">
-                           <img src="<?php echo $post['']; ?>" alt="">
+                            <span><strong>By:</strong> <?php echo $event['U_Username']; ?> <strong>@</strong><?php echo $event['EV_PO_DateTime']; ?></span><button>Join</button>
                         </div>
                     </div>
-                <?php endforeach; ?>
+            <?php endforeach; ?>
         </div>
-        <aside>
-            Display w/e data here
+        <aside class="event-img">
+            <h1>Event Images</h1>
+            <img src="img/events/1.jpg" alt="">
+            <img src="img/events/2.jpg" alt="">
+            <img src="img/events/3.jpg" alt="">
+            <img src="img/events/4.jpg" alt="">
+            <img src="img/events/5.jpg" alt="">
+            <img src="img/events/6.jpg" alt="">
+            <img src="img/events/7.jpg" alt="">
+            <img src="img/events/8.jpg" alt="">
+            <img src="img/events/9.jpg" alt="">
+            <img src="img/events/10.jpg" alt="">
+            <img src="img/events/11.jpg" alt="">
+            <img src="img/events/12.jpg" alt="">
+            <img src="img/events/13.jpg" alt="">
         </aside> 
     </main>
 <?php include('inc/footer.php'); ?>
